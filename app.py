c1b26eab25093f8d07ea61ac334ceb3a8119be60
Ingrid Venancio <ingrid.venancioo@gmail.com>
2026-03-12 23:31:09 -0300

    Relatório dos produtos falta arrumar e fazer relatório do listar

--- app.py
+++ app.py
@@ -8,16 +8,17 @@ def home():
 
 @app.route('/revisao')
 def revisao():
-    return '''
-<h1>Funcionou?</h1>
-<p>Sim, funcionou</p>
-<p style="color: red;">Ebaaa!</p>
-'''
+    nome = 'Ingrid'
+    return render_template('variaveis.html', idade=28, nome=nome)
+
 
 @app.route('/revisao/<nome>/<int:idade>')
 def parametros(nome,idade):
-    ano_nascimento = 2026 - idade
-    return 'Olá {nome}, você tem {idade} anos, porque nasceu em {ano_nascimento}.'
+    return render_template('variaveis.hmtl', idade=idade, nome=nome)
+
+@app.route('/produtos/cadastrar')
+def cadastrar_produtos():
+    return render_template('produtos/form-produto.html')
 
 
 
